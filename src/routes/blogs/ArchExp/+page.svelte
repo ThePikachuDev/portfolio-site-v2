a32d36dcc6ff7b3e4a7c7ce32dcb67a38317251e
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-03-06 20:40:40 +0530

    added metatags for blog

--- src/routes/blogs/ArchExp/+page.svelte
+++ src/routes/blogs/ArchExp/+page.svelte
@@ -5,7 +5,18 @@
 
 
 <div>
+    <!-- Twitter Meta Tags -->
+   <meta name="twitter:card" content="summary">
+   <meta name="twitter:url" content="https://pikachusite.vercel.app">
+   <meta name="twitter:title" content="The Pikachu">
+   <meta name="twitter:description" content="My Arch Linux Experience">
+   <meta name="twitter:image" content="/hyprwm3.png">
 
+
+   <link rel="canonical" href="https://pikachusite.vercel.app">
+   <meta name="theme-color" content="#111111">
+   <meta name="mobile-web-app-capable" content="yes">
+   <meta name="apple-mobile-web-app-capable" content="yes">
     <ArchExp />
 </div>
 
